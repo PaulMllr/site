4879fce911c1537867cc624cca54a81caf2eb513
Paul <lincore@ya.ru>
2018-11-02 11:26:53 +1000

    Jar resource getter fix

--- src/main/kotlin/io/codenetics/site/service/DataSourceService.kt
+++ src/main/kotlin/io/codenetics/site/service/DataSourceService.kt
@@ -7,16 +7,14 @@ package io.codenetics.site.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Service
-import java.io.File
 
-const val HOMEPAGE_RESOURCE_JSON_NAME = "homepage.json"
+const val HOMEPAGE_RESOURCE_JSON_NAME = "/homepage.json"
 
 @Service
 class DataSourceService {
 
     fun formHomePageData(): Map<String, *> {
-        val classLoader = javaClass.classLoader
-        val result = ObjectMapper().readValue(File(classLoader.getResource(HOMEPAGE_RESOURCE_JSON_NAME).file), HashMap::class.java)
+        val result = ObjectMapper().readValue(javaClass.getResourceAsStream(HOMEPAGE_RESOURCE_JSON_NAME), HashMap::class.java)
         @Suppress("UNCHECKED_CAST")
         return result as Map<String, *>
     }
