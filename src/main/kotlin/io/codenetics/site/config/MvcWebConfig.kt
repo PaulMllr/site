f7709c9d0fd00d5071b25304dad04bc4fde134b1
Paul <lincore@ya.ru>
2018-11-06 09:59:17 +1000

    Enable cache and add meta information

--- src/main/kotlin/io/codenetics/site/config/MvcWebConfig.kt
+++ src/main/kotlin/io/codenetics/site/config/MvcWebConfig.kt
@@ -1,9 +1,11 @@
 package io.codenetics.site.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
 
 /**
  *  Created by Pavel Laktiushkin on 04.10.2018
@@ -18,6 +20,7 @@ class MvcWebConfig : WebMvcConfigurer {
         registry
                 .addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/", "/static/", "file:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
     }
 
 }
