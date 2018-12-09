02a3be0a4cad21310f3b1a6d9828f334fd5ecfb0
PaulMllr <lincore@ya.ru>
2018-12-09 20:00:47 +1000

    Implement the projects page

--- src/main/kotlin/io/codenetics/site/controller/ProjectsController.kt
+++ src/main/kotlin/io/codenetics/site/controller/ProjectsController.kt
@@ -1,5 +1,7 @@
 package io.codenetics.site.controller
 
+import io.codenetics.site.service.DataSourceService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,8 +18,13 @@ const val PROJECTS_VIEW_NAME = "projects/projects"
 @RequestMapping("/projects")
 class ProjectsController {
 
+    @Autowired
+    lateinit var dataSourceService: DataSourceService
+
+
     @GetMapping
     fun getProjects(request: HttpServletRequest, model: Model): String {
+        model.addAllAttributes(dataSourceService.formProjectsPageData())
         return PROJECTS_VIEW_NAME
     }
 }
--- src/main/kotlin/io/codenetics/site/service/DataSourceService.kt
+++ src/main/kotlin/io/codenetics/site/service/DataSourceService.kt
@@ -1,4 +1,3 @@
-
 package io.codenetics.site.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -9,12 +8,21 @@ import org.springframework.stereotype.Service
  */
 
 const val HOMEPAGE_RESOURCE_JSON_NAME = "/homepage.json"
+const val PROJECTS_RESOURCE_JSON_NAME = "/projects.json"
 
 @Service
 class DataSourceService {
 
     fun formHomePageData(): Map<String, *> {
-        val result = ObjectMapper().readValue(javaClass.getResourceAsStream(HOMEPAGE_RESOURCE_JSON_NAME), HashMap::class.java)
+        return formModelFromJson(HOMEPAGE_RESOURCE_JSON_NAME)
+    }
+
+    fun formProjectsPageData(): Map<String, *> {
+        return formModelFromJson(PROJECTS_RESOURCE_JSON_NAME)
+    }
+
+    private fun formModelFromJson(jsonPath: String): Map<String, *> {
+        val result = ObjectMapper().readValue(javaClass.getResourceAsStream(jsonPath), HashMap::class.java)
         @Suppress("UNCHECKED_CAST")
         return result as Map<String, *>
     }
